68986db60f5d7df1eb177bcc3774a2807a7a9220
SURAMERICANA\juliqupi <jquinchia@sura.com.co>
2023-03-18 12:48:32 -0500

    Avance

--- autenticacion/flaskr/vistas/vistas.py
+++ autenticacion/flaskr/vistas/vistas.py
@@ -5,6 +5,7 @@ from flask_restful import Resource
 from flask import jsonify
 from flask_jwt_extended import create_access_token
 from datetime import datetime
+import jwt
 
 
 class VistaAutenticar(Resource):
@@ -16,11 +17,15 @@ class VistaAutenticar(Resource):
             return jsonify({"msg": "Bad username or password"}), 401
 
         access_token = create_access_token(identity=username)
-        fecha_actual = datetime.now()     
+        fecha_actual = datetime.now()
         fecha_y_hora_en_texto = fecha_actual.strftime('%d/%m/%Y %H:%M')
-        return {'mensaje': "login exitoso",
-                'token_acceso': access_token,
-                'fechaEmision': fecha_y_hora_en_texto,
-                'fechaExpiracion': "",
-                'rol': "Vendedor"
-                }
+
+        secreto = access_token
+        payload = {'user_id': 1,
+                   'fechaEmision': fecha_y_hora_en_texto,
+                   'fechaExpiracion': '',
+                   'rol': "vendedor"
+                   }
+
+        token = jwt.encode(payload, secreto, algorithm='HS256')
+        return token
