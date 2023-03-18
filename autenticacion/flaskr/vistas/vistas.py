40638eeeb3729573b000141df9762b503393503a
SURAMERICANA\juliqupi <jquinchia@sura.com.co>
2023-03-18 13:29:44 -0500

    Avances

--- autenticacion/flaskr/vistas/vistas.py
+++ autenticacion/flaskr/vistas/vistas.py
@@ -3,9 +3,10 @@ from flask import request
 from ..modelos import db
 from flask_restful import Resource
 from flask import jsonify
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 from datetime import datetime
-import jwt
+
+secreto = 'frase-secreta'
 
 
 class VistaAutenticar(Resource):
@@ -14,18 +15,30 @@ class VistaAutenticar(Resource):
         username = request.json.get("username")
         password = request.json.get("password")
         if username != "test" or password != "test":
-            return jsonify({"msg": "Bad username or password"}), 401
+            return ({"message": "Bad username or password"}), 401
 
-        access_token = create_access_token(identity=username)
         fecha_actual = datetime.now()
         fecha_y_hora_en_texto = fecha_actual.strftime('%d/%m/%Y %H:%M')
 
-        secreto = access_token
         payload = {'user_id': 1,
                    'fechaEmision': fecha_y_hora_en_texto,
                    'fechaExpiracion': '',
                    'rol': "vendedor"
                    }
+        token = create_access_token(
+            identity=username, additional_claims=payload)
+        return jsonify(access_token=token)
 
-        token = jwt.encode(payload, secreto, algorithm='HS256')
-        return token
+    @jwt_required()
+    def get(self):
+        try:
+            current_user = get_jwt_identity()
+            # claims = get_jwt_claims()
+            print('----token', current_user)
+            # print('----------payload', claims)
+            # continuar con la lógica del servicio
+            return {'message':'token valido', 'userid':current_user},200
+        except jwt.ExpiredSignatureError:
+            return {'message': 'token valido'}, 200
+        except jwt.InvalidTokenError:
+            return {'message': 'token invalido'}, 400
