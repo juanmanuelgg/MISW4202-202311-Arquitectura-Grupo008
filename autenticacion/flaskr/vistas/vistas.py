1296b5d76b262f16162197eb35f531da30e9a3bb
Juan Manuel González Garzón <jmgonzalezgarzon@gmail.com>
2023-03-19 19:08:25 -0500

    logica de lista negra segun intentos fallidos

--- autenticacion/flaskr/vistas/vistas.py
+++ autenticacion/flaskr/vistas/vistas.py
@@ -7,16 +7,49 @@ from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identi
 from datetime import datetime
 
 secreto = 'frase-secreta'
+limite_de_intentos = 10
 
 
 class VistaAutenticar(Resource):
+    # TODO: Recuperar estas estructuras de un archivo o una tabla
+    lista_negra = set()
+    intentos_visitante = dict()
 
     def post(self):
+        # 1. Obtener información del visitante
+        visitor_ip = ''
+        taken_from = ''
+        if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+            visitor_ip = request.environ['REMOTE_ADDR']
+            taken_from = 'REMOTE_ADDR'
+        else:
+            # if behind a proxy
+            visitor_ip = request.environ['HTTP_X_FORWARDED_FOR']
+            taken_from = 'HTTP_X_FORWARDED_FOR'
+
+        key_visitante = "{} {}".format(taken_from, visitor_ip)
+        if key_visitante in self.lista_negra:
+            return ({"message": "Blacklisted."}), 401
+
+        # 2. Verificar al visitante contra la lista negra.
+
+        # 3. Proceder con el intento de autenticación y aumentar el contador de intentos si falla o reset si es exitoso.
+        # 3.1. Evaluar la cantidad de intentos fallidos y evaluar si entra a lista negra.
         username = request.json.get("username")
         password = request.json.get("password")
         if username != "test" or password != "test":
-            return ({"message": "Bad username or password"}), 401
+            if key_visitante in self.intentos_visitante:
+                self.intentos_visitante[key_visitante] = self.intentos_visitante[key_visitante] + 1
+            else:
+                self.intentos_visitante[key_visitante] = 1
 
+            if self.intentos_visitante[key_visitante] >= limite_de_intentos:
+                self.lista_negra.add(key_visitante)
+            return ({"message": "Bad username or password"}), 401
+        else:
+            self.intentos_visitante[key_visitante] = 0
+
+        # 4. Generar el token de autenticación.
         fecha_actual = datetime.now()
         fecha_y_hora_en_texto = fecha_actual.strftime('%d/%m/%Y %H:%M')
 
